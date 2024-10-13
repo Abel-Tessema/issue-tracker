29771093a529581e15f67c618dcd32404cab5b36
Béla Jash <abelxotessema@gmail.com>
2024-10-13 13:45:49 +0300

    Style the IssueDetailsPage.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import prisma from "@/prisma/client";
 import {notFound} from "next/navigation";
+import {Card, Flex, Heading, Text} from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 
 interface Props {
   params: {id: string}
 }
 
 async function IssueDetailsPage({params: {id}}: Props) {
-  if (typeof id !== 'number') notFound(); /* Even though the IDE may say 'id' cannot have type 'number',
-                                             it still works. It does redirect them to a 404 page. */
+  const issueId = parseInt(id);
+  if (isNaN(issueId)) notFound();
   
-  const issue = await prisma.issue.findUnique({where: {id: parseInt(id)}});
+  const issue = await prisma.issue.findUnique({where: {id: issueId}});
   
   if (!issue) notFound();
   
   return (
-    <>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
-    </>
+    <div className='space-y-5'>
+      <Heading>{issue.title}</Heading>
+      <Flex gapX='5'>
+        <IssueStatusBadge status={issue.status}/>
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <Text>{issue.description}</Text>
+      </Card>
+    </div>
   );
 }
 

f146e95939086cc7731bfe32e6a0860414b1ef90
Béla Jash <abelxotessema@gmail.com>
2024-10-13 14:13:52 +0300

    Add markdown preview for issue descriptions.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import {notFound} from "next/navigation";
 import {Card, Flex, Heading, Text} from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import Markdown from "react-markdown";
 
 interface Props {
   params: {id: string}
@@ -23,8 +24,8 @@ async function IssueDetailsPage({params: {id}}: Props) {
         <IssueStatusBadge status={issue.status}/>
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <Text>{issue.description}</Text>
+      <Card className='prose'>
+        <Text><Markdown>{issue.description}</Markdown></Text>
       </Card>
     </div>
   );
